458f9d1686c145778ad8d2539c3aaba15e325408
Nicolas Florentin <nicolas@sleede.com>
2022-03-02 13:26:40 +0100

    version 0.0.5

--- lib/openlab/client.rb
+++ lib/openlab/client.rb
@@ -9,22 +9,23 @@ module Openlab
 
     def initialize(app_secret: nil)
       @app_secret = app_secret || Openlab.config.app_secret
-      app_secret = @app_secret
 
+      self.class.default_options[:verify] = Openlab.config.httparty_verify
+      self.class.default_options[:verify_peer] = Openlab.config.httparty_verify_peer
       self.class.base_uri Openlab.config.base_uri
-      self.class.headers 'Accept' => 'application/json', 'Authorization' => "Token token=#{app_secret}"
+      self.class.headers 'Accept' => 'application/json', 'Authorization' => "Token token=#{@app_secret}"
     end
 
     def projects
       @projects ||= Openlab::Projects.new(self)
     end
 
-    def method_missing(method_name, *arguments, &block)
-      self.class.respond_to?(method_name) ? self.class.__send__(method_name, *arguments, &block) : super
-    end
+    # def method_missing(method_name, *arguments, &block)
+    #   self.class.respond_to?(method_name) ? self.class.__send__(method_name, *arguments, &block) : super
+    # end
 
-    def respond_to?(method_name, include_private = false)
-      self.class.respond_to?(method_name) || super
-    end
+    # def respond_to?(method_name, include_private = false)
+    #   self.class.respond_to?(method_name) || super
+    # end
   end
 end
--- lib/openlab_ruby.rb
+++ lib/openlab_ruby.rb
@@ -16,12 +16,14 @@ module Openlab
   end
 
   class Config
-    attr_accessor :app_secret, :base_uri
+    attr_accessor :app_secret, :base_uri, :httparty_verify, :httparty_verify_peer
     attr_reader :api_version_path
 
     def initialize
       @base_uri = 'https://openprojects.fab-manager.com'
       @api_version_path = '/api/v1'
+      @httparty_verify = true
+      @httparty_verify_peer = true
     end
   end
 end
